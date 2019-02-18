227f4473f4ef111ec2893e5f00161f9715661ca9
Brahim Loukil <bloukil@objectware.fr>
2019-02-18 18:53:31 +0100

    [INIT]

---
+++ spring-querydsl/src/main/java/fr/codeonce/labs/springquerydsl/SpringQuerydslApplication.java
@@ -0,0 +1,13 @@
+package fr.codeonce.labs.springquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuerydslApplication.class, args);
+	}
+
+}
---
+++ spring-querydsl/src/main/java/fr/codeonce/labs/springquerydsl/entities/Customer.java
@@ -0,0 +1,41 @@
+package fr.codeonce.labs.springquerydsl.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String fn) {
+		firstName = fn;
+	}
+
+	public void setLastName(String ln) {
+		lastName = ln;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
